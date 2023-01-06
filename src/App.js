f2a98a9071dae77018a9d3ed8d0e244926e92537
chirathdasanayaka <Zone@123>
2023-01-07 01:36:58 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar></Navbar>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
       <Footer></Footer>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,37 @@
 import React from 'react'
-import {NavLink, Link} from "react-router-dom"
+import {Link} from "react-router-dom"
 import './Navbar.css'
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+// toggleButton.addEventListener('click', () => {
+//   navbarLinks.classList.toggle('active')
+// })
 
 const Navbar = () => {
   return (
     <div>
       <nav class="navbar">
-        <div class="brand-title">Brand Name</div>
-        <a href="#" class="toggle-button">
+       <Link to="/home" style={{textDecoration: 'none'}}>
+        <div className="brand-title">Brand Name</div>
+       </Link>
+        <a href="/" class="toggle-button">
           <span class="bar"></span>
           <span class="bar"></span>
           <span class="bar"></span>
         </a>
         <div class="navbar-links">
           <ul>
-            <li><a>Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <Link to="/home" style={{textDecoration: 'none'}}>
+              <li>Home</li>
+            </Link>
+            <Link to="/about" style={{textDecoration: 'none'}}>
+              <li>About</li>
+            </Link>
+            <Link to="/contact" style={{textDecoration: 'none'}}>
+              <li>Contact</li>
+            </Link>
           </ul>
         </div>
       </nav>
